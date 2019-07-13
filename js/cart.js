34b0cd4a4d27776ca694f16cb33d42e98a458b58
闪电金刚之葫芦娃 <1402141265@qq.com>
2019-07-13 23:09:24 +0800

    '可以增加或减少商品数量并计算价格和数量'

--- js/cart.js
+++ js/cart.js
@@ -4,14 +4,15 @@ $(() => {
     // console.log(jsonStr)
     //判断，如果本地数据不为null
     //把数据转换为js格式
+    let arr;
     if (jsonStr !== null) {
-        let arr = JSON.parse(jsonStr);
+        arr = JSON.parse(jsonStr);
         //生成结构
         let html = '';
         // 遍历arr，生成结构
         arr.forEach(e => {
-            console.log(e)
-            html += `<div class="item" data-id="6">
+            // console.log(e)
+            html += `<div class="item" data-id="${e.pID}">
             <div class="row">
               <div class="cell col-1 row">
                 <div class="cell col-1">
@@ -49,25 +50,95 @@ $(() => {
         $('.empty-tip').hide();
         $('.cart-header').removeClass('hidden');
         $('.total-of').removeClass('hidden');
+        total();
     }
 
     //全选和多选
     //设置全选
-    $('.pick-all').on('click',function(){
+    $('.pick-all').on('click', function () {
         let status = $(this).prop('checked');
         // console.log(status)
-        $('.item-ck').prop('checked',status);
-        $('.pick-all').prop('checked',status);
+        $('.item-ck').prop('checked', status);
+        $('.pick-all').prop('checked', status);
+        total();
     })
     //多选
-    $('.item-ck').on('click',()=>{
+    $('.item-ck').on('click', () => {
         // console.log($('.item-ck:checked').length);
         // if($('.item-ck:checked').length === $('.item-ck').length){
         //     $('.pick-all').prop('checked',true);
         // }else{
         //     $('.pick-all').prop('checked',false);
         // }
-        $('.pick-all').prop('checked',$('.item-ck:checked').length === $('.item-ck').length);
+        $('.pick-all').prop('checked', $('.item-ck:checked').length === $('.item-ck').length);
+        total();
     })
 
+    //计算总价和总数量
+    function total() {
+        //总价
+        let totalPrices = 0;
+        //总数量
+        let totalQuantity = 0;
+        //获取选中的商品进行计算
+        $('.item-ck:checked').each((i, e) => {
+            let id = parseInt($(e).parents('.item').attr('data-id'));
+            // console.log(id);
+            arr.forEach(e => {
+                if (e.pID === id) {
+                    totalPrices += e.number * e.price;
+                    totalQuantity += e.number;
+                }
+                $('.total-money').text(totalPrices);
+                $('.selected').text(totalQuantity);
+            })
+        })
+    }
+    function a(){
+        
+    }
+    //添加商品数量
+    $('.item').on('click', '.add', function () {
+        //获取点击前的旧数量
+        let oldNum = $(this).siblings('.number').val();
+        //点一下就加加一次
+        oldNum++;
+        //把加完后的新值添加到页面上
+        $(this).siblings('.number').val(oldNum);
+        $('.computed').text(oldNum * $('.price').text())
+        //获取当前点击的元素的data-id值
+        let id = parseInt($(this).parents('.item').attr('data-id'));
+        //把和本都数据里的id相等的对象筛选出来
+        let obj = arr.find(e => {
+            return e.pID === id;
+        })
+        //给筛选出来的对象赋上新的数值
+        obj.number = oldNum;
+        // console.log(arr)
+        jsonStr = JSON.stringify(arr);
+        localStorage.setItem('shopCartData', jsonStr);
+        total();
+    })
+    //减少商品数量
+    $('.reduce').on('click',function(){
+        //获取点击前的旧数量
+        let oldNum = $(this).siblings('.number').val();
+        //点一下就加加一次
+        oldNum--;
+        //把加完后的新值添加到页面上
+        $(this).siblings('.number').val(oldNum);
+        $('.computed').text(oldNum * $('.price').text())
+        //获取当前点击的元素的data-id值
+        let id = parseInt($(this).parents('.item').attr('data-id'));
+        //把和本都数据里的id相等的对象筛选出来
+        let obj = arr.find(e => {
+            return e.pID === id;
+        })
+        //给筛选出来的对象赋上新的数值
+        obj.number = oldNum;
+        // console.log(arr)
+        jsonStr = JSON.stringify(arr);
+        localStorage.setItem('shopCartData', jsonStr);
+        total();
+    })
 })
